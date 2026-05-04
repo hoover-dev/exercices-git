02a3d24be473186d2f01eb4542c979ff1e7f547a
Paeneach Hoover <joebden147@gmail.com>
2026-05-04 05:55:40 +0000

    semaine 2 jour 1 : variables Javascript

---
+++ variables.js
@@ -0,0 +1,14 @@
+// Semaine 2 Jour 1 - Variables JavaScript
+// Paenéach Hoover - ESAG-NDE Lomé
+
+const nom = "Paenéach";
+const ecole = "ESAG-NDE";
+let annee = 1;
+const objectif = "Toronto, Canada";
+const anneeNaissance = 2026 - 21;
+
+console.log("Nom : " + nom);
+console.log("École : " + ecole);
+console.log("Année L" + annee);
+console.log("Né en : " + anneeNaissance);
+console.log("Objectif : " + objectif);
