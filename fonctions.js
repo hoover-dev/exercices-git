5f96523f26ea64ee9e8f727c517f7cf9016dbc75
Paeneach Hoover <joebden147@gmail.com>
2026-05-05 06:15:29 +0000

    semaine 2 jour 2 : fonction JS

---
+++ fonctions.js
@@ -0,0 +1,15 @@
+// Semaine 2 Jour 2 - Fonctions JavaScript
+
+function saluer(prenom) {
+  console.log("Bonjour " + prenom + " !");
+}
+
+function additionner(a, b) {
+  return a + b;
+}
+
+const multiplier = (a, b) => a * b;
+
+saluer("Paenéach");
+console.log(additionner(5, 3));
+console.log(multiplier(3, 4));
