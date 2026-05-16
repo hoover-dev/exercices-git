a599ad98a349336347980da0fd7ad7072d7ddc59
Paeneach Hoover <joebden147@gmail.com>
2026-05-16 21:08:18 +0000

    feat: boucles for, while, do...while

---
+++ boucles.js
@@ -0,0 +1,18 @@
+// BLOC 1 - for
+for (let i = 0; i < 5; i++) {
+    console.log("Tour numéro :", i);
+}
+
+// BLOC 2 - while
+let solde = 10000;
+while (solde > 0) {
+    solde -= 2500;
+    console.log("reste :", solde);
+}
+
+// BLOC 3 - do...while
+let solde2 = 10000;
+do {
+    solde2 -= 2500;
+    console.log("tour numero :", solde2);
+} while (solde2 > 0);
